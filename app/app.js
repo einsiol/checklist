35d7af04002339f9573dae61a7ab6bda5d24039f
Einar Ólafsson <einsiol@gmail.com>
2017-03-11 16:50:30 +0100

    adding a authentication

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-var checklistApp = angular.module('BlankApp', ['ngMaterial', 'firebase']);
+var checklistApp = angular.module('BlankApp', ['ngMessages', 'ngMaterial', 'ngCookies', 'ngRoute', 'firebase']);
 
 
 checklistApp.directive('myEnter', function () {
@@ -15,26 +15,160 @@ checklistApp.directive('myEnter', function () {
     };
 });
 
+checklistApp.config(function($routeProvider, $locationProvider) {
+  $routeProvider
+   .when('/login', {
+    templateUrl: 'login.html',
+    controller: 'LoginController',
+     })
+   .when('/register', {
+    templateUrl: 'createAccount.html',
+    controller: 'LoginController',
+   })
+   .otherwise({
+      templateUrl: 'checklists.html',
+    });
+
+  // configure html5 to get links working on jsfiddle
+  //$locationProvider.html5Mode(true);
+});
+
+var checklistData = firebase.database().ref().child('checklist');
+var userData;
+var presenceRef;
+var onDisconnectRef;
+//checklistData.onDisconnect().set("I disconnected!");
+
+checklistApp.controller('MainController', function($scope, $rootScope, $timeout, $mdDialog, $cookies, $location) {
+
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+         // console.log(user.displayName);
+         $cookies.put('user', user.displayName);
+
+         console.log(user)
+
+         $rootScope.user = $cookies.get('user');
+
+         checkIfUserIsLoggedIn();
+
+      } else {
+        $cookies.remove('user');
+         checkIfUserIsLoggedIn();
+      }
+    }, function(error){
+        console.log(error)
+    });
+
+   
+   // console.log($rootScope.user)
+   function checkIfUserIsLoggedIn() {
+     if (!$rootScope.user && $location.path() != '/register'){
+
+        $location.path('/login');
+        //console.log('redirect');
+
+    } else {
+        usersData = firebase.database().ref('users/' + $rootScope.user);
+        presenceRef = firebase.database().ref('users/' + $rootScope.user + '/disconnectmessage');
+
+    }
+
+    console.log($rootScope.user)
+
+    if($rootScope.user && $location.path() == '/login') {
+        $location.path('/');
+        //console.log('redirect if logged in')
+    }
+
+   }
+
+    
+
+    //console.log($rootScope.user);
+    
+ });
+
+checklistApp.controller('LoginController', function($scope, $rootScope, $timeout, $mdDialog, $cookies, $location) {
+
+    $scope.registerUser = function(){
+
+        $scope.serverError = null;
+
+        firebase.auth().createUserWithEmailAndPassword($scope.email, $scope.password).then(function(user) {
+            //console.log('')
+
+            user.updateProfile({
+                displayName: $scope.name
+            }).then(function() {
+                 $cookies.put('user', firebase.auth().currentUser.displayName);
+                 $rootScope.user = firebase.auth().currentUser.displayName;
+
+                 console.log('success')
+
+                 $location.path('/');
+                 
+            }, function(error) {
+                // An error happened.
+            });        
+
+            
+           
+            
+           // logUser(user); // Optional
+        }, function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+
+            $scope.serverError = error.message;
+        });
+    }
+
+    $scope.serverError = 'my message';
+
+    $scope.loginUser = function(){
+        firebase.auth().signInWithEmailAndPassword($scope.email, $scope.password).then(function(user) {
+    
+            $cookies.put('user', user.displayName);
+            $rootScope.user = user.displayName;
+
+            $location.path('/');
+          
+            
+           
+            
+           // logUser(user); // Optional
+        }, function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+
+            $scope.showErrorMessage = true;
+            $scope.serverError = error.message;
+
+            console.log(error.message)
+        });
+    }
+    
+ });
 
 
 
-checklistApp.controller('ListCtrl', function($scope, $timeout, $mdDialog, $firebaseObject, $firebaseArray) {
+checklistApp.controller('ListCtrl', function($scope, $rootScope, $timeout, $mdDialog, $firebaseObject, $firebaseArray) {
 
-   	 var ref = firebase.database().ref().child('checklist');
+   	 
 	  // download the data into a local object
-	 $scope.items = $firebaseArray(ref);
+	 $scope.items = $firebaseArray(checklistData);
+
+     //items = $scope.items;
 	 
-
-    $scope.removeItem = function(id){
-
-    	$scope.items.$remove(id);
-    };
 
     $scope.addToList = function(){
 
     	if ($scope.listItem) {
 
-	    	$scope.items.$add({name:$scope.listItem, wanted:false});
+	    	$scope.items.$add({name:$scope.listItem, wanted:false, user:$rootScope.user});
     	}
 
     	$scope.listItem = null;
